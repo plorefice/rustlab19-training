402ab84cecff0b6a387eeb3993460a7ffcbe17f1
Pietro Lorefice <pietro@develer.com>
2019-05-06 10:45:52 +0200

    ex1: improve exercise with several more cases

--- src/ex1.rs
+++ src/ex1.rs
@@ -1,5 +1,29 @@
-/* Exercise 1 - Hello World! */
+// Exercise 1 - println!
 
 fn main() {
-    /* Use the println!() macro to write something to stdout */
+    // let is how variables are declared in Rust. More on this later!
+    let x = 2;
+    let y = 3;
+
+    // This writes "Hello World!" to stdout
+    println!("Hello World!");
+
+    // Like many other languages, println! accepts a formatter string
+    // and a variable number of arguments containing expressions
+    // to be evaluated and printed.
+    println!("The answer to life, the universe and everything is {}", 42);
+
+    // EX: Use println! to print "x + y = 5" using the x and y above.
+
+    // Advanced formatting can be performed by using the {:} syntax.
+    // More on this at https://doc.rust-lang.org/std/fmt/index.html
+
+    // EX: Try printing a number in decimal, hexadecimal, octal and binary format.
+
+    // Rust has recently introduced dbg!() to perform quick printf-debugging.
+    // This is documented at https://doc.rust-lang.org/std/macro.dbg.html
+
+    // EX: Modify the statement below to debug the value of x * y.
+    let z = (x * y) + x;
+    println!("z is {}", z);
 }
